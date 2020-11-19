6032641958f7972f8c4ca0b8eaaab60d725ac0e9
Tobias S <44494591+BruceJi7@users.noreply.github.com>
2020-11-19 18:04:51 +0900

    Tidy up regex process to give cleaner results

--- naverDictScraper.py
+++ naverDictScraper.py
@@ -13,6 +13,7 @@ SPACE_RX = re.compile(r'\s+')
 
 DE_KOREAN_RX = re.compile(r'\([^A-Za-z]+\)')
 DE_ENGLISH_RX = re.compile(r'[A-Za-z]+')
+MATCH_KOREAN_RX = re.compile(r'[\uAC00-\uD7AF]+')
 
 HANJA_RX = re.compile(r'[\u4e00-\u9fff]+')
 
@@ -25,19 +26,13 @@ ADVERB_RX = re.compile(r'\[부사\]')
 
 class WordIdiomWord():
 
-    def __init__(self, searched_word, word_cell, definitions_cell):
+    def __init__(self, searched_word, language, word_cell, definitions_cell):
         self.searched_word = searched_word
         self.word_cell = word_cell
         self.definitions_cell = definitions_cell
+        self.language = language
 
-    @property
-    def language(self):
-        english_present = DE_ENGLISH_RX.search(self.searched_word)
-        if english_present:
-            return 'English'
-        else:
-            return 'Korean'
-    
+   
     @property
     def result_type(self):
         return 'word_idiom'
@@ -49,7 +44,7 @@ class WordIdiomWord():
 
     @property
     def hanja(self):
-        if self.language == 'Korean':
+        if self.language == 'Korean': #If the searched word is Korean, remove Korean from the definition
             main_word = self.searched_word
 
             #Extract whole word span
@@ -79,7 +74,10 @@ class WordIdiomWord():
             return None
 
         #Eliminate Korean brackets from definition
-        meaning_span = DE_KOREAN_RX.sub('', meaning_span).strip().capitalize()
+        if self.language == 'Korean':
+            meaning_span = DE_KOREAN_RX.sub('', meaning_span).strip().capitalize()
+        else:
+            meaning_span = DE_ENGLISH_RX.sub('', meaning_span)
 
         #Replace Korean parts of speech with English ones:
         if '[' in meaning_span:
@@ -87,6 +85,8 @@ class WordIdiomWord():
             meaning_span = VERB_RX.sub('Verb:', meaning_span)
             meaning_span = ADJECTIVE_RX.sub('Adj:', meaning_span)
             meaning_span = ADVERB_RX.sub('Adv:', meaning_span)
+
+        meaning_span = SPACE_RX.sub(' ', meaning_span)
 
         return meaning_span
 
@@ -102,20 +102,21 @@ class WordIdiomWord():
         }
 
 class MeaningsWord():
-    def __init__(self, searched_word, word_cell, definitions_cell):
+    def __init__(self, searched_word, language, word_cell, definitions_cell):
         self.definitions_cell = definitions_cell
         self.word_cell = word_cell
         self.searched_word = searched_word
         self.result_word = searched_word.capitalize()     
         self.hanja = None
+        self.language = language
 
-    @property
-    def language(self):
-        english_present = bool(DE_ENGLISH_RX.search(self.searched_word))
-        if english_present == True: #False, because the boolean is for what is REMAINING
-            return 'English'
-        else:
-            return 'Korean'
+    # @property
+    # def language(self):
+    #     english_present = bool(DE_ENGLISH_RX.search(self.searched_word))
+    #     if english_present == True: #False, because the boolean is for what is REMAINING
+    #         return 'English'
+    #     else:
+    #         return 'Korean'
 
     @property
     def result_type(self):
@@ -133,11 +134,27 @@ class MeaningsWord():
 
         meaning_span = self.definitions_cell.find('span', class_="fnt_e07 _ttsText")
         if meaning_span:
-            if self.language == 'Korean':
-                return f'{main_word.capitalize()}, as in {meaning_span.text}'
-            else:
-                return f'"{meaning_span.text}" 에서와 같이 {main_word}'
 
+            meaning_span = meaning_span.text
+
+            if self.language == 'English':
+                
+                meaning_span = DE_ENGLISH_RX.sub('', meaning_span)
+                meaning_span = SPACE_RX.sub(' ', meaning_span)
+
+                return f'"{meaning_span}" 에서와 같이 {main_word}'
+                
+            else:
+
+                
+                meaning_span = DE_KOREAN_RX.sub('', meaning_span).strip().capitalize()
+                meaning_span = SPACE_RX.sub(' ', meaning_span)
+                
+    
+                if meaning_span:
+                    return f'{main_word.capitalize()}, as in "{meaning_span}"'
+                else:
+                    return main_word.capitalize()
 
         else:
             return main_word.capitalize()
@@ -157,14 +174,22 @@ class MeaningsWord():
 
 def getDefinition(word):
 
+    word_language = None
+    korean_present = MATCH_KOREAN_RX.search(word)
+    if korean_present:
+        word_language = 'Korean'
+    else:
+        word_language = 'English'
+
+
     # Make Word Objects out of word-idiom section
-    def word_idioms_to_objects(html_section):
+    def word_idioms_to_objects(html_section, word_language):
 
         naver_word_results = html_section.find_all('dt')
 
         word_objects = []
         for w in naver_word_results:
-            w_object = WordIdiomWord(word, w, w.find_next_sibling('dd'))
+            w_object = WordIdiomWord(word, word_language, w, w.find_next_sibling('dd'))
             w_dict_format = w_object.dictify
 
             word_objects.append(w_dict_format)
@@ -173,12 +198,12 @@ def getDefinition(word):
 
     
     #Make Word Objects out of Meanings section
-    def meanings_to_objects(html_section):
+    def meanings_to_objects(html_section, word_language):
         word_titles = html_section.find_all('dt')
         word_objects = []
 
         for w in word_titles:
-            w_object = MeaningsWord(word, w, w.find_next_sibling('dd'))
+            w_object = MeaningsWord(word, word_language, w, w.find_next_sibling('dd'))
             w_dict_format = w_object.dictify
 
             word_objects.append(w_dict_format)
@@ -201,12 +226,12 @@ def getDefinition(word):
 
     if sections:
         #Sections[0] corresponds to 'words & idioms'
-        word_idiom_section_word_objects = word_idioms_to_objects(sections[0])
+        word_idiom_section_word_objects = word_idioms_to_objects(sections[0], word_language)
 
     # If the word has a 'meanings' section - not all do
     if len(sections) > 1:
         #Sections[1] corresponds to 'meanings'
-        meanings_section_word_objects =  meanings_to_objects(sections[1])
+        meanings_section_word_objects =  meanings_to_objects(sections[1], word_language)
 
     combined_word_objects = word_idiom_section_word_objects + meanings_section_word_objects
     combined_word_objects_with_id = []
@@ -253,7 +278,7 @@ def load_words_from_file():
     print(words)
     return words
 
-# word_results = getDefinition('화제')
+word_results = getDefinition('화제')
 # word_results = getDefinition('friend')
 
 # loaded_words = load_words_from_file()
